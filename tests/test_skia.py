59fd4c3f5c6307b43e0841f3080bffd1a9ee9296
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2020-03-25 17:51:31 +0900

    Add test files

---
+++ tests/test_skia.py
@@ -0,0 +1,32 @@
+import pytest
+import skia
+
+
+def draw(canvas):
+    from math import cos, sin
+    scale = 256.
+    R = 0.45 * scale
+    TAU = 6.2831853
+
+    path = skia.Path()
+    path.moveTo(R, 0.)
+    for i in range(7):
+        theta = 3 * i * TAU / 7
+        path.lineTo(R * cos(theta), R * sin(theta))
+    path.close()
+
+    paint = skia.Paint()
+    paint.setAntiAlias(True)
+
+    canvas.clear(0xFFFFFFFF)
+    canvas.translate(0.5 * scale, 0.5 * scale)
+    canvas.drawPath(path, paint)
+
+
+def test_skia():
+    surface = skia.Surface(256, 256)
+    canvas = surface.getCanvas()
+    draw(canvas)
+    image = surface.makeImageSnapshot()
+    data = image.encodeToData()
+    encoded = bytes(data)
