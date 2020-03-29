7e58a518eed3d8564d44e5d382ca37b0760bdad1
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2020-03-29 10:11:37 +0900

    add explicit nullptr check in getCanvas

--- src/skia/Surface.cpp
+++ src/skia/Surface.cpp
@@ -15,8 +15,10 @@ py::class_<SkSurface, sk_sp<SkSurface>>(m, "Surface")
             throw std::runtime_error("Failed to allocate surface.");
         return surface;
     }))
-    .def("getCanvas", [](SkSurface& surface) {
-        auto canvas = surface.getCanvas();
+    .def("getCanvas", [](sk_sp<SkSurface>& surface) {
+        if (surface.get() == nullptr)
+            throw std::runtime_error("Null pointer exception.");
+        auto canvas = surface->getCanvas();
         if (canvas == nullptr)
             throw std::runtime_error("Failed to get canvas.");
         return canvas;
