47cdd255190ee251061ac696015e23d4b0b80ed3
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2020-03-29 10:27:07 +0900

    [WIP] debug getCanvas call in windows

--- src/skia/Surface.cpp
+++ src/skia/Surface.cpp
@@ -15,15 +15,12 @@ py::class_<SkSurface, sk_sp<SkSurface>>(m, "Surface")
             throw std::runtime_error("Failed to allocate surface.");
         return surface;
     }))
-    .def("getCanvas", [](sk_sp<SkSurface>& surface) {
-        if (surface.get() == nullptr)
-            throw std::runtime_error("Null pointer exception.");
-        auto canvas = surface->getCanvas();
-        if (canvas == nullptr)
-            throw std::runtime_error("Failed to get canvas.");
-        return canvas;
-    },
-        py::return_value_policy::reference)
+    // .def("getCanvas", &SkSurface::getCanvas,
+    //     py::return_value_policy::reference)
+    .def("getCanvas", [](SkSurface& surface) -> bool {
+        surface.getCanvas();
+        return true;
+    })
     .def("makeImageSnapshot",
         py::overload_cast<>(&SkSurface::makeImageSnapshot),
         "Returns SkImage capturing SkSurface contents.")
