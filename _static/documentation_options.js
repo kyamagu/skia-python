7d9c95cb2f0bb652af4c3b5af4a04093e605d89d
kyamagu <kyamagu@users.noreply.github.com>
2020-11-19 06:03:33 +0000

    deploy: f3efbcbfce513a80d591e7bdecfeda33bc3019ef

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '86.0',
+    VERSION: '86.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
