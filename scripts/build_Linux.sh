a61ad7415e0ea3d0e5ddea7b3be28bcab380d310
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2021-08-31 11:25:58 +0900

    Use unspecified python in build

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -18,14 +18,14 @@ export LDFLAGS="-lrt"
 git clone https://gn.googlesource.com/gn && \
     cd gn && \
     git checkout 981f46c64d1456d2083b1a2fa1367e753e0cdc1b && \
-    python2 build/gen.py && \
+    python build/gen.py && \
     ninja -C out gn && \
     cd ..
 
 # Build skia
 cd skia && \
     patch -p1 < ../patch/git-sync-deps.patch && \
-    python2 tools/git-sync-deps && \
+    python tools/git-sync-deps && \
     cp -f ../gn/out/gn bin/gn && \
     bin/gn gen out/Release --args='
 is_official_build=true
