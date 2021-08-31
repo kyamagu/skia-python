0c255ba78ec222fcefdebdd88f210870cf100fef
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2021-08-31 12:00:04 +0900

    Add patch to macos build

--- scripts/build_macOS.sh
+++ scripts/build_macOS.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-shopt -s expand_aliases
-alias python="python2"
-
 # Build skia
 export CC=clang
 export CXX=clang++
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 
+
+function apply_patch {
+    patch -p1 < ../patch/find_xcode_sysroot.patch;
+    2to3 -w third_party/externals/icu/scripts/make_data_assembly.py;
+}
+
+
 cd skia && \
     python tools/git-sync-deps && \
-    bin/gn gen out/Release --args='
+    apply_patch && \
+    bin/gn gen out/Release --args="
 is_official_build=true
 skia_enable_tools=true
 skia_use_system_libjpeg_turbo=false
@@ -18,7 +23,7 @@ skia_use_system_libwebp=false
 skia_use_system_libpng=false
 skia_use_system_icu=false
 skia_use_system_harfbuzz=false
-extra_cflags_cc=["-frtti", "-mmacosx-version-min=10.9"]
-' && \
+extra_cflags_cc=[\"-frtti\", \"-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}\"]
+" && \
     ninja -C out/Release skia skia.h experimental_svg_model && \
     cd ..
