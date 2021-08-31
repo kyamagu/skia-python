29ad4335586582e500493cd4cda6d1cddb2e45e1
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2021-08-31 11:11:16 +0900

    merge ci workflow files

---
+++ scripts/build_Linux.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Install system dependencies
+yum --disablerepo=epel install -y \
+    fontconfig-devel \
+    mesa-libGL-devel \
+    xorg-x11-server-Xvfb \
+    mesa-dri-drivers && \
+    yum clean all && \
+    rm -rf /var/cache/yum
+
+# Build gn
+export CC=gcc
+export CXX=g++
+export AR=ar
+export CFLAGS="-Wno-deprecated-copy"
+export LDFLAGS="-lrt"
+git clone https://gn.googlesource.com/gn && \
+    cd gn && \
+    git checkout 981f46c64d1456d2083b1a2fa1367e753e0cdc1b && \
+    python2 build/gen.py && \
+    ninja -C out gn && \
+    cd ..
+
+# Build skia
+cd skia && \
+    patch -p1 < ../patch/git-sync-deps.patch && \
+    python2 tools/git-sync-deps && \
+    cp -f ../gn/out/gn bin/gn && \
+    bin/gn gen out/Release --args='
+is_official_build=true
+skia_enable_tools=true
+skia_use_system_libjpeg_turbo=false
+skia_use_system_libwebp=false
+skia_use_system_libpng=false
+skia_use_system_icu=false
+skia_use_system_harfbuzz=false
+extra_cflags_cc=["-frtti"]
+extra_ldflags=["-lrt"]
+' && \
+    ninja -C out/Release skia skia.h experimental_svg_model && \
+    cd ..
---
+++ scripts/build_macOS.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Build skia
+export CC=clang
+export CXX=clang++
+export MACOSX_DEPLOYMENT_TARGET="10.9"
+cd skia && \
+    python2 tools/git-sync-deps && \
+    bin/gn gen out/Release --args='
+is_official_build=true
+skia_enable_tools=true
+skia_use_system_libjpeg_turbo=false
+skia_use_system_libwebp=false
+skia_use_system_libpng=false
+skia_use_system_icu=false
+skia_use_system_harfbuzz=false
+extra_cflags_cc=["-frtti", "-mmacosx-version-min=10.9"]
+' && \
+    ninja -C out/Release skia skia.h experimental_svg_model && \
+    cd ..
