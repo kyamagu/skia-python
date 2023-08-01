31f9cd2dc4de38421eb8a3ff1fe6d3bfb329229a
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-01 05:17:09 +0100

    m113: SkImage::CompressionType -> SkTextureCompressionType

--- src/skia/GrContext_mock.cpp
+++ src/skia/GrContext_mock.cpp
@@ -1,11 +1,13 @@
 #include "common.h"
 #include <include/gpu/mock/GrMockTypes.h>
+#include <include/gpu/GrBackendSurface.h>
+#include <include/core/SkTextureCompressionType.h>
 
 void initGrContext_mock(py::module &m) {
 
 py::class_<GrMockTextureInfo>(m, "GrMockTextureInfo")
     .def(py::init<>())
-    .def(py::init<GrColorType, SkImage::CompressionType, int>(),
+    .def(py::init<GrColorType, SkTextureCompressionType, int>(),
         py::arg("colorType"), py::arg("compressionType"), py::arg("id"))
     .def("__eq__", &GrMockTextureInfo::operator==, py::is_operator())
     .def("getBackendFormat", &GrMockTextureInfo::getBackendFormat)
