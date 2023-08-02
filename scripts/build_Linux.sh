2067f34c2e2f992f26952ac3c1190ae5f4e4a219
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-02 06:11:55 +0100

    Adjusting to m91+ way of building the SVG module

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -49,7 +49,7 @@ cd skia && \
     cp -f ../gn/out/gn bin/gn && \
     bin/gn gen out/Release --args="
 is_official_build=true
-skia_enable_tools=true
+skia_enable_svg=true
 skia_use_system_libjpeg_turbo=false
 skia_use_system_libwebp=false
 skia_use_system_libpng=false
@@ -58,5 +58,5 @@ skia_use_system_harfbuzz=false
 extra_cflags_cc=[\"-frtti\"]
 extra_ldflags=[\"-lrt\"]
 " && \
-    ninja -C out/Release skia skia.h experimental_svg_model && \
+    ninja -C out/Release && \
     cd ..
