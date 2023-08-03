9a06317615612fb1ffccb0551a222aa64a5c3cbd
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-03 22:38:08 +0100

    Exposure COLRv1 functionality to skia-python + freetype-py's use

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -46,6 +46,7 @@ git clone https://gn.googlesource.com/gn && \
 # Build skia
 cd skia && \
     patch -p1 < ../patch/skia-m116-minimize-download.patch && \
+    patch -p1 < ../patch/skia-m116-colrv1-freetype.diff && \
     python3 tools/git-sync-deps && \
     cp -f ../gn/out/gn bin/gn && \
     bin/gn gen out/Release --args="
