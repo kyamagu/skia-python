f241ac2b9ee56b35b486aa2c13ed9e4f35443a99
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-04 08:14:52 +0100

    SkFilterMode is used in the newer SkPicture::makeShader() in m116

--- src/skia/SamplingOptions.cpp
+++ src/skia/SamplingOptions.cpp
@@ -4,6 +4,13 @@
 void initSamplingOptions(py::module& m) {
     py::class_<SkSamplingOptions> samplingoptions(m, "SamplingOptions");
 
+        py::enum_<SkFilterMode>(m, "FilterMode")
+        .value("kNearest", SkFilterMode::kNearest,
+            "single sample point (nearest neighbor)")
+        .value("kLinear", SkFilterMode::kLinear,
+            "interporate between 2x2 sample points (bilinear interpolation)")
+        .export_values();
+
     samplingoptions
         .def(py::init<>());
 }
