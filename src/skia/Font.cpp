2702abec0387ab32406ee9dc5de03abb7c3d2242
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-04 19:36:51 +0100

    Okay, so this only works on Linux. Fair enough.

--- src/skia/Font.cpp
+++ src/skia/Font.cpp
@@ -3,9 +3,11 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 #include <pybind11/iostream.h>
+#ifdef __linux__
 #include <freetype/freetype.h>
 #include <freetype/ftcolor.h>
 #include <src/ports/SkFontHost_FreeType_common.h>
+#endif
 
 using Axis = SkFontParameters::Variation::Axis;
 using Coordinate = SkFontArguments::VariationPosition::Coordinate;
@@ -819,6 +821,7 @@ font
         )docstring",
         py::arg("typeface"), py::arg("size"), py::arg("scaleX"),
         py::arg("skewX"))
+#ifdef __linux___
     .def_static("COLRv1Bound",
         py::overload_cast<FT_Face, SkGlyphID, SkRect*>(&SkScalerContext_FreeType_Base::computeColrV1GlyphBoundingBox),
         R"docstring(
@@ -829,6 +832,7 @@ font
         R"docstring(
         )docstring",
         py::arg("canvas"), py::arg("face"), py::arg("glyphId"), py::arg("palette_index"), py::arg("rootTransform"))
+#endif
     .def("__eq__", &SkFont::operator==,
         R"docstring(
         Compares :py:class:`Font` and font, and returns true if they are
