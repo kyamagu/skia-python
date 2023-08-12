f5dff2e657df630260e19afe136ea11088adb910
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-08-12 01:56:25 +0100

    Turn out that Mac/Windows only needs expat, extra from Linux. CI failure on windows
    Trying to avoid windows running out of disk space.

--- scripts/build_Windows.sh
+++ scripts/build_Windows.sh
@@ -4,6 +4,7 @@ export PATH="${PWD}/depot_tools:$PATH"
 
 # Build skia
 cd skia && \
+    patch -p1 < ../patch/skia-m117-minimize-download.patch && \
     patch -p1 < ../patch/skia-m116-colrv1-freetype.diff && \
     python tools/git-sync-deps && \
     bin/gn gen out/Release --args='
