3d2f5758459da762cb5b82ce79c223214d31577c
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-04-22 22:18:05 +0100

    Adding more SamplingOptions init tests

--- tests/test_samplingoptions.py
+++ tests/test_samplingoptions.py
@@ -1,5 +1,22 @@
 import skia
 import pytest
 
-def test_SamplingOptions_init():
-    assert isinstance(skia.SamplingOptions(), skia.SamplingOptions)
+@pytest.mark.parametrize('args', [
+    (),
+    # FilterQuality enum Equivalents
+    (skia.FilterMode.kLinear,  skia.MipmapMode.kNearest),  # FilterQuality.kMedium_SkFilterQuality CPU
+    (skia.FilterMode.kLinear,  skia.MipmapMode.kLinear),   # FilterQuality.kMedium_SkFilterQuality GPU
+    (skia.FilterMode.kLinear,  skia.MipmapMode.kNone),     # FilterQuality.kLow_SkFilterQuality
+    (skia.FilterMode.kNearest, skia.MipmapMode.kNone)      # FilterQuality.kNone_SkFilterQuality
+])
+def test_SamplingOptions_init(args):
+    assert isinstance(skia.SamplingOptions(*args), skia.SamplingOptions)
+
+def test_SamplingOptions_init_cubic1():
+    # FilterQuality.kHigh_SkFilterQuality
+    assert isinstance(skia.SamplingOptions(skia.CubicResampler.Mitchell()), skia.SamplingOptions)
+
+
+def test_SamplingOptions_init_cubic2():
+    # No FilterQuality Equivalent
+    assert isinstance(skia.SamplingOptions(skia.CubicResampler.CatmullRom()), skia.SamplingOptions)
