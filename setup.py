e04470b42944718c7e5c74adc45725de124eb04a
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-04-29 18:19:18 +0100

    m125: libskunicode split into libskunicode_core and libskunicode_icu

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ if sys.platform == 'win32':
     ]
     EXTRA_OBJECTS = list(
     ) + [os.path.join(SKIA_OUT_PATH, 'svg.lib'), os.path.join(SKIA_OUT_PATH, 'skresources.lib'), os.path.join(SKIA_OUT_PATH, 'skia.lib'),
-         os.path.join(SKIA_OUT_PATH, 'skshaper.lib'), os.path.join(SKIA_OUT_PATH, 'skunicode.lib')]
+         os.path.join(SKIA_OUT_PATH, 'skshaper.lib'),
+         os.path.join(SKIA_OUT_PATH, 'unicode_core.lib'), os.path.join(SKIA_OUT_PATH, 'unicode_icu.lib')]
     EXTRA_COMPILE_ARGS = [
         '/std:c++17',  # c++20 fails.
         '/DVERSION_INFO=%s' % __version__,
@@ -64,7 +65,8 @@ elif sys.platform == 'darwin':
     ]
     EXTRA_OBJECTS = list(
     ) + [os.path.join(SKIA_OUT_PATH, 'libsvg.a'), os.path.join(SKIA_OUT_PATH, 'libskia.a'),
-         os.path.join(SKIA_OUT_PATH, 'libskshaper.a'), os.path.join(SKIA_OUT_PATH, 'libskunicode.a')]
+         os.path.join(SKIA_OUT_PATH, 'libskshaper.a'),
+         os.path.join(SKIA_OUT_PATH, 'libskunicode_core.a'), os.path.join(SKIA_OUT_PATH, 'libskunicode_icu.a')]
     EXTRA_COMPILE_ARGS = [
         '-std=c++17',
         '-stdlib=libc++',
@@ -97,7 +99,8 @@ else:
     ]
     EXTRA_OBJECTS = list(
     ) + [os.path.join(SKIA_OUT_PATH, 'libsvg.a'), os.path.join(SKIA_OUT_PATH, 'libskresources.a'), os.path.join(SKIA_OUT_PATH, 'libskia.a'),
-         os.path.join(SKIA_OUT_PATH, 'libskshaper.a'), os.path.join(SKIA_OUT_PATH, 'libskunicode.a')]
+         os.path.join(SKIA_OUT_PATH, 'libskshaper.a'),
+         os.path.join(SKIA_OUT_PATH, 'libskunicode_core.a'), os.path.join(SKIA_OUT_PATH, 'libskunicode_icu.a')]
     EXTRA_COMPILE_ARGS = [
         '-std=c++17',
         '-fvisibility=hidden',
