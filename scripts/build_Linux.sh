14c6d68aeca8a6b113f5988cc9926b2beb8666ca
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-07-27 18:06:58 +0100

    skia m128 patch plus adjustments

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -60,7 +60,7 @@ git clone https://gn.googlesource.com/gn && \
 
 # Build skia
 cd skia && \
-    patch -p1 < ../patch/skia-m127-minimize-download.patch && \
+    patch -p1 < ../patch/skia-m128-minimize-download.patch && \
     patch -p1 < ../patch/skia-m123-colrv1-freetype.diff && \
     python3 tools/git-sync-deps && \
     cp -f ../gn/out/gn bin/gn && \
