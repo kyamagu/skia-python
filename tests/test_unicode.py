eca5ba4061a9745a39e2e6bd6e2bbd19c3157c6d
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-10-01 20:19:33 +0100

    Re-enable Unicodes-related tests affected by icudtl.dat
    Revert "skia.Unicodes.ICU.Make() returns "None" on windows. Disabling skia.Unicodes tests on windows for the moment."
    
    This reverts commit 01e9fc5795bc0bbedc3d3a3c0af3187ad59b8551.
    
    Revert "missing import sys"
    
    This reverts commit 39b6e0db9925320050354c9b46987f15f517e35a.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -1,20 +1,13 @@
 import skia
 import pytest
-import sys
 
 def test_Unicodes_init0():
-    if sys.platform.startswith("win"):
-        pytest.skip("Known not to work; To be investigated.")
     assert isinstance(skia.Unicode(), skia.Unicode)
 
 # recommended alias (upstream):
 def test_Unicodes_init1():
-    if sys.platform.startswith("win"):
-        pytest.skip("Known not to work; To be investigated.")
     assert isinstance(skia.Unicodes.ICU.Make(), skia.Unicode)
 
 # Canonical
 def test_Unicodes_init2():
-    if sys.platform.startswith("win"):
-        pytest.skip("Known not to work; To be investigated.")
     assert isinstance(skia.Unicode.ICU_Make(), skia.Unicode)
