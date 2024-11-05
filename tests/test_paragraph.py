892d9437cb16696ffab25579594a7c59d942bff8
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-11-05 18:47:52 +0000

    New Paragraph test, adapted from #278; also refactoring existing tests.

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -1,14 +1,29 @@
 import skia
 import pytest
 
-def test_FontCollection_init0():
-    assert isinstance(skia.textlayout.FontCollection(), skia.textlayout_FontCollection)
+@pytest.fixture(scope='session')
+def textlayout_font_collection():
+    return skia.textlayout.FontCollection()
 
-def test_ParagraphStyle_init0():
-    assert isinstance(skia.textlayout.ParagraphStyle(),skia.textlayout_ParagraphStyle)
+def test_FontCollection_init0(textlayout_font_collection):
+    assert isinstance(textlayout_font_collection, skia.textlayout_FontCollection)
 
-def test_TextStyle_init0():
-    assert isinstance(skia.textlayout.TextStyle(), skia.textlayout_TextStyle)
+
+@pytest.fixture(scope='session')
+def paragraph_style():
+    return skia.textlayout.ParagraphStyle()
+
+def test_ParagraphStyle_init0(paragraph_style):
+    assert isinstance(paragraph_style, skia.textlayout_ParagraphStyle)
+
+
+@pytest.fixture(scope='session')
+def textlayout_text_style():
+    return skia.textlayout.TextStyle()
+
+def test_TextStyle_init0(textlayout_text_style):
+    assert isinstance(textlayout_text_style, skia.textlayout_TextStyle)
+
 
 @pytest.fixture(scope='session')
 def paragraph_builder():
@@ -23,3 +38,26 @@ def test_Paragraph_init0(paragraph_builder):
     paragraph_builder.addText("")
     paragraph = paragraph_builder.Build()
     assert isinstance(paragraph, skia.textlayout_Paragraph)
+
+
+# Adapted from #278, to make sure that "\n" results in a line break  (rather that .notdef).
+# Height is larger than twice width, if a break happens.
+def test_Paragraph_linebreak(paragraph_builder, textlayout_text_style, textlayout_font_collection, paragraph_style):
+    paint = skia.Paint()
+    paint.setColor(skia.ColorBLACK)
+    paint.setAntiAlias(True)
+
+    textlayout_text_style.setFontSize(50)
+    textlayout_text_style.setForegroundPaint(paint)
+
+    textlayout_font_collection.setDefaultFontManager(skia.FontMgr())
+
+    builder = skia.textlayout.ParagraphBuilder.make(
+        paragraph_style, textlayout_font_collection, skia.Unicodes.ICU.Make()
+    )
+    builder.pushStyle(textlayout_text_style)
+
+    builder.addText("o\no")
+    paragraph = builder.Build()
+    paragraph.layout(300)
+    assert (paragraph.Height > 0) and (paragraph.Height > paragraph.LongestLine * 2)
