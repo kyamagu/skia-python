be383cf0fa194e434b93f056282d53e8f1c9ea2a
HinTak <HinTak@users.noreply.github.com>
2024-11-14 12:43:30 +0000

    deploy: db7d7fcc84dc5e59ea8a925457f2d319a57052df

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '130.0b10',
+    VERSION: '132.0b11',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
