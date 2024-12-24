4b9f3023b2b8a7f90f32474cec9dad5b022806d2
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-12-24 21:24:23 +0000

    Patch skia-m132-egl-runtime.diff into Linux build

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -61,6 +61,7 @@ git clone https://gn.googlesource.com/gn && \
 cd skia && \
     patch -p1 < ../patch/skia-m132-minimize-download.patch && \
     patch -p1 < ../patch/skia-m132-colrv1-freetype.diff && \
+    patch -p1 < ../patch/skia-m132-egl-runtime.diff && \
     python3 tools/git-sync-deps && \
     cp -f ../gn/out/gn bin/gn && \
     bin/gn gen out/Release --args="
