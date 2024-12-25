d6a296376b953d4240e57f1fad7f406c8680f45f
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-12-25 04:44:16 +0000

    Missing EGL/egl.h header from libglvnd-devel
    On EL7, EGL/egl.h is in mesa-libEGL-devel instead of libglvnd-devel (later Redhat)
    
    Missing GLES2/gl2.h - in mesa-libGLES-devel
    
    It is not likely that the runtime (libgGL*) libraries are missing if the devel are installed, but you never know.
    Include them also.

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -16,6 +16,7 @@ if [[ $EUID -eq 0 ]]; then
         python3 \
         fontconfig-devel \
         mesa-libGL-devel \
+        mesa-libEGL-devel libglvnd-devel mesa-libGLES-devel libglvnd mesa-libGLES mesa-libEGL libglvnd-egl \
         xorg-x11-server-Xvfb \
         mesa-dri-drivers && \
         yum clean all && \
