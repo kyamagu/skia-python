2f55c72170486dcddecf8ed3da8d62c511509dc7
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-05-25 20:22:41 +0100

    Apparently target_cpu="arm64" is needed for arm windows build
    Conflicts:
    	scripts/build_Windows.sh

--- scripts/build_Windows.sh
+++ scripts/build_Windows.sh
@@ -2,12 +2,20 @@
 
 export PATH="${PWD}/depot_tools:$PATH"
 
+if [[ ${RUNNER_ARCH} == "ARM64" ]]; then
+    EXTRA_ARGS='
+target_cpu="arm64"
+'
+    echo "setting ARM64 Windows Skia build"
+fi
+
+
 # Build skia
 cd skia && \
     python tools/git-sync-deps && \
     patch -p1 < ../patch/make_data_assembly.patch && \
     patch -p1 < ../patch/skia-m87-c++-code.diff && \
-    bin/gn gen out/Release --args='
+    bin/gn gen out/Release --args="
 is_official_build=true
 skia_enable_tools=true
 skia_use_system_libjpeg_turbo=false
@@ -17,7 +25,8 @@ skia_use_system_icu=false
 skia_use_system_harfbuzz=false
 skia_use_system_expat=false
 skia_use_system_zlib=false
-extra_cflags_cc=["/GR", "/EHsc", "/MD"]
-' && \
+extra_cflags_cc=[\"/GR\", \"/EHsc\", \"/MD\"]
+${EXTRA_ARGS}
+" && \
     ninja -C out/Release skia skia.h experimental_svg_model && \
     cd ..
